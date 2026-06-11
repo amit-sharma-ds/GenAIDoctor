9834909ec2b726665bf64372c148cf3f6996af70
Amit Sharma <137687459+amit-sharma-ds@users.noreply.github.com>
2026-06-11 23:28:44 +0530

    commit

--- voice_of_the_patient.py
+++ voice_of_the_patient.py
@@ -1,5 +1,5 @@
 # if you dont use pipenv uncomment the following:
-# from dotenv import load_dotenv
+# from dotenv import load_doten
 # load_dotenv()
 
 #Step1: Setup Audio recorder (ffmpeg & portaudio)
